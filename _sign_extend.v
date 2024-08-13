40a8b5ad799f905510b29c691d5c07999cf3a2fd
sarthak-sharmaa <147803893+sarthak-sharmaa@users.noreply.github.com>
2024-08-13 16:49:42 +0530

    Update _sign_extend.v

--- _sign_extend.v
+++ _sign_extend.v
@@ -1,7 +1,7 @@
 module Sign_Extend (In,Imm_Ext,ImmSrc);
 
     input [31:0]In;
-    input ImmSrc;
+    input ImmSrc;  // 1 means store word, 0 means load word
     output [31:0]Imm_Ext;
 
     assign Imm_Ext = (ImmSrc == 1'b1) ? ({{20{In[31]}},In[31:25],In[11:7]}):
